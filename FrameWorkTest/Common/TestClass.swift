f1a432dd5ddfc35914d9d21cdb29311f168fd8cd
Arcilite <tork.the.knight@gmail.com>
2014-09-16 14:39:07 +0400

    add project

---
+++ FrameWorkTest/Common/TestClass.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Arcilite on 16.09.14.
+//
+//
+
+import Foundation
+
+
+class TestClass: NSObject {
+    override init(){
+       super.init()
+        println("TestClass");
+    }
+}
---
+++ FrameWorkTest/FrameWorkTest/FrameWorkTest/FrameWorkTest.h
@@ -0,0 +1,19 @@
+//
+//  FrameWorkTest.h
+//  FrameWorkTest
+//
+//  Created by Arcilite on 16.09.14.
+//  Copyright (c) 2014 Ramotion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameWorkTest.
+FOUNDATION_EXPORT double FrameWorkTestVersionNumber;
+
+//! Project version string for FrameWorkTest.
+FOUNDATION_EXPORT const unsigned char FrameWorkTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameWorkTest/PublicHeader.h>
+
+
